c76374ebb3dbf07fa4782b1bfd2c26989a24796d
Pernichev <pernichevm@gmail.com>
2019-07-14 20:06:19 +0300

    added logger

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'mail@mail.com',
+        status: "active"
+    },
+    {
+        id: 2,
+        name: 'Johnatan Doeh',
+        email: 'mail@mailuh.com',
+        status: "inactive"
+    },
+    {
+        id: 3,
+        name: 'J D',
+        email: 'jd@jdoh.com',
+        status: "active"
+    },
+    {
+        id: 4,
+        name: 'Last Doe',
+        email: 'last@doe.com',
+        status: "inactive"
+    },
+]
+
+module.exports = members;
